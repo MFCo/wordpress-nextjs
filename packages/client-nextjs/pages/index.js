0a228c1d3cf16e6dd68918c5bb6641c04e0e5b58
mfc-boost <marianococirio@gmail.com>
2018-01-12 14:30:14 -0300

    first approach

--- packages/client-nextjs/pages/index.js
+++ packages/client-nextjs/pages/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import fetch from 'isomorphic-unfetch';
 
-function HomePage() {
+const HomePage = ({ recentPosts }) => {
+  console.log(recentPosts);
   return (
     <div>
       HI!
     </div>
   );
-}
+};
+
+HomePage.propTypes = {
+  recentPosts: PropTypes.any
+};
+
+HomePage.getInitialProps = async function () {
+  const recentPosts = await fetch('http://localhost:8000/wp-json/wp/v2/posts');
+  const jsonRP = await recentPosts.json();
+  return {
+    recentPosts: jsonRP
+  };
+};
 
 export default HomePage;
